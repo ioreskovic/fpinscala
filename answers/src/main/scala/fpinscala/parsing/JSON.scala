09268eae12c1669de03218a133a37b16ea0a8043
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-09-01 23:41:06 +0200

    Applied scalafmt

--- answers/src/main/scala/fpinscala/parsing/JSON.scala
+++ answers/src/main/scala/fpinscala/parsing/JSON.scala
@@ -6,30 +6,30 @@ import language.implicitConversions
 trait JSON
 
 object JSON {
-  case object JNull extends JSON
-  case class JNumber(get: Double) extends JSON
-  case class JString(get: String) extends JSON
-  case class JBool(get: Boolean) extends JSON
-  case class JArray(get: IndexedSeq[JSON]) extends JSON
+  case object JNull                          extends JSON
+  case class JNumber(get: Double)            extends JSON
+  case class JString(get: String)            extends JSON
+  case class JBool(get: Boolean)             extends JSON
+  case class JArray(get: IndexedSeq[JSON])   extends JSON
   case class JObject(get: Map[String, JSON]) extends JSON
 
-  def jsonParser[Parser[+_]](P: Parsers[Parser]): Parser[JSON] = {
+  def jsonParser[Parser[+ _]](P: Parsers[Parser]): Parser[JSON] = {
     // we'll hide the string implicit conversion and promote strings to tokens instead
     // this is a bit nicer than having to write token everywhere
     import P.{string => _, _}
     implicit def tok(s: String) = token(P.string(s))
 
-    def array = surround("[","]")(
-      value sep "," map (vs => JArray(vs.toIndexedSeq))) scope "array"
-    def obj = surround("{","}")(
-      keyval sep "," map (kvs => JObject(kvs.toMap))) scope "object"
+    def array =
+      surround("[", "]")(value sep "," map (vs => JArray(vs.toIndexedSeq))) scope "array"
+    def obj =
+      surround("{", "}")(keyval sep "," map (kvs => JObject(kvs.toMap))) scope "object"
     def keyval = escapedQuoted ** (":" *> value)
     def lit = scope("literal") {
       "null".as(JNull) |
-      double.map(JNumber(_)) |
-      escapedQuoted.map(JString(_)) |
-      "true".as(JBool(true)) |
-      "false".as(JBool(false))
+        double.map(JNumber(_)) |
+        escapedQuoted.map(JString(_)) |
+        "true".as(JBool(true)) |
+        "false".as(JBool(false))
     }
     def value: Parser[JSON] = lit | obj | array
     root(whitespace *> (obj | array))
@@ -37,8 +37,8 @@ object JSON {
 }
 
 /**
- * JSON parsing example.
- */
+  * JSON parsing example.
+  */
 object JSONExample extends App {
   val jsonTxt = """
 {
@@ -69,7 +69,7 @@ object JSONExample extends App {
   val P = fpinscala.parsing.Reference
   import fpinscala.parsing.ReferenceTypes.Parser
 
-  def printResult[E](e: Either[E,JSON]) =
+  def printResult[E](e: Either[E, JSON]) =
     e.fold(println, println)
 
   val json: Parser[JSON] = JSON.jsonParser(P)
