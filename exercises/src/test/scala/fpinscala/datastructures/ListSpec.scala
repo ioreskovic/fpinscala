9c182b33a606e96054db07404484ff5e68c590c1
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-09-02 22:39:27 +0200

    Reworded test spec

--- exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -5,21 +5,17 @@ import fpinscala.datastructures.List.tail
 class ListSpec extends WordSpec {
 
 	"A List" when {
-		"it is empty" should {
-			"produce NoSuchElementException when tail is invoked" in {
+		"invoking tail" should {
+			"produce NoSuchElementException when it is empty" in {
 				assertThrows[NoSuchElementException] {
 					tail(List())
 				}
 			}
-		}
 
-		"it is singleton" should {
-			"produce empty list when tail is invoked" in {
+			"produce empty list when it has only one element" in {
 				assert(tail(List(1)) == List())
 			}
-		}
 
-		"it has more than one element" should {
 			"produce another list with all elements in order without head" in {
 				assert(tail(List(0, 1, 2, 3)) == List(1, 2, 3))
 			}
