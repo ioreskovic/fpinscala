be88b577971b9824596e0d3d683b29e0096db4c1
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-09-02 22:47:14 +0200

    List operation: set head

--- exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -1,6 +1,6 @@
 import org.scalatest.WordSpec
 import fpinscala.datastructures.List
-import fpinscala.datastructures.List.tail
+import fpinscala.datastructures.List._
 
 class ListSpec extends WordSpec {
 
@@ -18,6 +18,22 @@ class ListSpec extends WordSpec {
 
 			"produce another list with all elements in order without head" in {
 				assert(tail(List(0, 1, 2, 3)) == List(1, 2, 3))
+			}
+		}
+
+		"setting head" should {
+			"produce NoSuchElementException list when invoked on empty list" in {
+				assertThrows[NoSuchElementException] {
+					setHead(List(), 1)
+				}
+			}
+
+			"produce singleton list when invoked on singleton list" in {
+				assert(setHead(List(0), 1) == List(1))
+			}
+
+			"produce tail of original list with prepended element" in {
+				assert(setHead(List(0, 2, 3, 4), 1) == List(1, 2, 3, 4))
 			}
 		}
 	}
