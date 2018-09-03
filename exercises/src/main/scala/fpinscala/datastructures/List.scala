9baa47ada6a95a24499b314ce99bbbf47f1413af
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-09-03 23:05:19 +0200

    List operations: tail-recursive foldLeft (no idea how to test this)

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -82,7 +82,11 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def length[A](l: List[A]): Int = foldRight(l, 0)((_, len) => len + 1)
 
-  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @annotation.tailrec
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil         => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
   def map[A, B](l: List[A])(f: A => B): List[B] = ???
 
