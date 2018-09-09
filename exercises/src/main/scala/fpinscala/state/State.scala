a4c81fba91414416372d7ca217f851565c83225a
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-09-09 17:53:40 +0200

    Completed Exercises 6.5, 6.6
      * RNG#map
      * RNG#map2

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -39,6 +39,8 @@ object RNG {
     case (i, nextRng) => (i / (Int.MaxValue.toDouble + 1), nextRng)
   }
 
+  def double2: Rand[Double] = map(nonNegativeInt)(_ / (Int.MaxValue.toDouble + 1))
+
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
     val (i, rng2) = rng.nextInt
     val (d, rng3) = double(rng2)
@@ -66,7 +68,17 @@ object RNG {
         (i :: acc, r2)
     }
 
-  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, rng2) = ra(rng)
+    val (b, rng3) = rb(rng2)
+    (f(a, b), rng3)
+  }
+
+  def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] = map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+  
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
   def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
 
