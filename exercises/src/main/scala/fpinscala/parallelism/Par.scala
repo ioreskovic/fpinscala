242307e8ee50065b856a5ca38cefa80efb1fa079
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-09-10 22:53:30 +0200

    Completed Exercise 7.3
    This was somewhat a c/p from hint/answer, since I didn't expect a new type.
    (Although, I should have, since we're only describing computations).
    
    Also, I'm not sure if I'm testing this properly. We'll see soon, I guess :)

--- exercises/src/main/scala/fpinscala/parallelism/Par.scala
+++ exercises/src/main/scala/fpinscala/parallelism/Par.scala
@@ -19,12 +19,41 @@ object Par {
     def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = // `map2` doesn't evaluate the call to `f` in a separate logical thread, in accord with our design choice of having `fork` be the sole function in the API for controlling parallelism. We can always do `fork(map2(a,b)(f))` if we want the evaluation of `f` to occur in a separate thread.
-    (es: ExecutorService) => {
-      val af = a(es)
-      val bf = b(es)
-      UnitFuture(f(af.get, bf.get)) // This implementation of `map2` does _not_ respect timeouts, and eagerly waits for the returned futures. This means that even if you have passed in "forked" arguments, using this map2 on them will make them wait. It simply passes the `ExecutorService` on to both `Par` values, waits for the results of the Futures `af` and `bf`, applies `f` to them, and wraps them in a `UnitFuture`. In order to respect timeouts, we'd need a new `Future` implementation that records the amount of time spent evaluating `af`, then subtracts that time from the available time allocated for evaluating `bf`.
+  private case class Map2Future[A, B, C](a: Future[A],
+                                         b: Future[B],
+                                         f: (A, B) => C)
+      extends Future[C] {
+    @volatile var cache: Option[C] = None
+
+    def isDone      = cache.isDefined
+    def isCancelled = a.isCancelled || b.isCancelled
+    def cancel(evenIfRunning: Boolean): Boolean =
+      a.cancel(evenIfRunning) || b.cancel(evenIfRunning)
+
+    def get = compute(Long.MaxValue)
+    def get(timeout: Long, units: TimeUnit): C =
+      compute(TimeUnit.NANOSECONDS.convert(timeout, units))
+
+    private def compute(timeoutNanos: Long): C = cache match {
+      case Some(c) => c
+      case None => {
+        val startA  = System.nanoTime
+        val aResult = a.get(timeoutNanos, TimeUnit.NANOSECONDS)
+        val stopA   = System.nanoTime
+        val timeA   = stopA - startA
+        val bResult = b.get(timeoutNanos - timeA, TimeUnit.NANOSECONDS)
+        val cResult = f(aResult, bResult)
+        cache = Some(cResult)
+        cResult
+      }
     }
+  }
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = es => {
+    val af = a(es)
+    val bf = b(es)
+    Map2Future(af, bf, f)
+  }
 
   def fork[A](a: => Par[A]): Par[A] = // This is the simplest and most natural implementation of `fork`, but there are some problems with it--for one, the outer `Callable` will block waiting for the "inner" task to complete. Since this blocking occupies a thread in our thread pool, or whatever resource backs the `ExecutorService`, this implies that we're losing out on some potential parallelism. Essentially, we're using two threads when one should suffice. This is a symptom of a more serious problem with the implementation, and we will discuss this later in the chapter.
     es =>
---
+++ exercises/src/test/scala/fpinscala/parallelism/ParSpec.scala
@@ -0,0 +1,19 @@
+import org.scalatest._
+import org.scalatest.prop._
+import org.scalacheck.Gen
+import fpinscala.parallelism.Par
+import fpinscala.parallelism.Par._
+
+import java.util.concurrent.{ Callable, Future, ExecutorService, Executors }
+
+class ParSpec extends AsyncWordSpec with Matchers with GeneratorDrivenPropertyChecks {
+	val es: ExecutorService = Executors.newCachedThreadPool()
+
+	"map2" should {
+		"eventually combine results of 2 futures" in {
+			val parC = Par.map2(Par.unit(1), Par.unit(2.0))((i, d) => (i + d).toString)
+
+			parC(es).get should be ("3.0")
+		}
+	}
+}
