9ded33f728c8149c3900f3d7d504b166d1d3704c
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-09-23 23:59:27 +0200

    Completed Exercise 9.7

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -12,20 +12,25 @@ trait Parsers[Parser[+ _]] { self => // so inner classes may call methods of tra
   def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   implicit def string(s: String): Parser[String]
+
   implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+
   implicit def asStringParser[A](a: A)(
-      implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+      implicit f: A => Parser[String]): ParserOps[String] =
+    ParserOps(f(a))
 
   def char(c: Char): Parser[Char] =
     string(c.toString).map(_.charAt(0))
 
-  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A] = ???
+  // primitive?
+  def or[A](p1: Parser[A], p2: => Parser[A]): Parser[A]
 
   def map[A, B](a: Parser[A])(f: A => B): Parser[B] = ???
 
   def many[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _).or(succeed(Nil))
 
+  // Shouldnt this be a primitive, like unit
   def succeed[A](a: A): Parser[A] =
     string("") map (_ => a)
 
@@ -34,16 +39,25 @@ trait Parsers[Parser[+ _]] { self => // so inner classes may call methods of tra
   def many1[A](p: Parser[A]): Parser[List[A]] =
     map2(p, many(p))(_ :: _)
 
-  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] = ???
+  def product[A, B](p: Parser[A], p2: => Parser[B]): Parser[(A, B)] =
+    map2(p, p2)((_, _))
 
   def map2[A, B, C](p: Parser[A], p2: => Parser[B])(f: (A, B) => C): Parser[C] =
-    product(p, p2).map(f.tupled)
+    for {
+      a <- p
+      b <- p2
+    } yield f(a, b)
 
   def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]] =
     if (n <= 0)
       succeed(List())
     else
       map2(p, listOfN(n - 1, p))(_ :: _)
+
+  // primitive
+  def flatMap[A, B](p: Parser[A])(f: A => Parser[B]): Parser[B]
+
+  implicit def regex(r: Regex): Parser[String]
 
   case class ParserOps[A](p: Parser[A]) {
     def |[B >: A](p2: => Parser[B]): Parser[B] =
@@ -59,8 +73,12 @@ trait Parsers[Parser[+ _]] { self => // so inner classes may call methods of tra
 
     def **[B](p2: => Parser[B]): Parser[(A, B)] =
       self.product(p, p2)
+
     def product[B](p2: => Parser[B]): Parser[(A, B)] =
       self.product(p, p2)
+
+    def flatMap[B](f: A => Parser[B]): Parser[B] =
+      self.flatMap(p)(f)
   }
 
   object Laws {
