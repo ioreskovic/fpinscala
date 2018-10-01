9798f69823e4d721c46011cf5827c45dcd07f5fb
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-10-01 23:40:15 +0200

    Completed exercise 10.16
    Also, it appears I completed 10.15 earlier accidentaly xD

--- exercises/src/main/scala/fpinscala/monoids/Monoid.scala
+++ exercises/src/main/scala/fpinscala/monoids/Monoid.scala
@@ -192,7 +192,10 @@ object Monoid {
   }
 
   def productMonoid[A, B](A: Monoid[A], B: Monoid[B]): Monoid[(A, B)] =
-    ???
+    new Monoid[(A, B)] {
+      def zero                     = (A.zero, B.zero)
+      def op(x: (A, B), y: (A, B)) = (A.op(x._1, y._1), B.op(x._2, y._2))
+    }
 
   def functionMonoid[A, B](B: Monoid[B]): Monoid[A => B] =
     ???
