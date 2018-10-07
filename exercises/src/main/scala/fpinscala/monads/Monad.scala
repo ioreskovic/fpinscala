14355d9d948f447c4a8556f7d8489e214e722b17
Ivan Oreskovic <ivan.oreskovic@gmail.com>
2018-10-07 11:20:33 +0200

    Completed exercise 11.8
    This felt like cheating xD

--- exercises/src/main/scala/fpinscala/monads/Monad.scala
+++ exercises/src/main/scala/fpinscala/monads/Monad.scala
@@ -74,7 +74,8 @@ trait Monad[M[_]] extends Functor[M] {
     a => flatMap(f(a))(g)
 
   // Implement in terms of `compose`:
-  def _flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] = ???
+  def _flatMap[A, B](ma: M[A])(f: A => M[B]): M[B] =
+    compose((_: Unit) => ma, f)(())
 
   def join[A](mma: M[M[A]]): M[A] = ???
 
